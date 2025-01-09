9c918431a2515c41a4367645c2cf25534a8b5c48
notarkhit <notarkhit@gmail.com>
2025-01-09 12:35:28 +0530

    update minMax.sh

---
+++ scripts1/minMax.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+clear
+echo "Enter the number of elements in the list"
+read n
+large=0
+min=999
+i=1
+
+while [ $i -le $n ]
+do
+	echo "enter a number"
+	read num
+	
+	if [ $num -gt $large ]
+	then
+		large=$num
+	fi
+
+	if [ $num -lt $min ] 
+	then
+		min=$num
+	fi
+
+done
+
+echo "minumum number is : $min"
+echo "maximum number is : $large"
